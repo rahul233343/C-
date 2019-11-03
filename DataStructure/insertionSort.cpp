46e3d021f7fe74257d9d283923442e0450dd7fdd
Rahul Sharma <rahul233343@gmail.com>
2019-11-03 22:45:29 +0530

    Without swap for loop is added but unable to figure out the time taken by both the for loops

--- DataStructure/insertionSort.cpp
+++ DataStructure/insertionSort.cpp
@@ -10,7 +10,7 @@ int main(){
 
     std::cout<<"Enter the size of vector"<<std::endl;
     std::cin>>size;
-    srand(time(0));
+//    srand(time(0));
 
     for(int i = 0; i <= size; i++){
         elements.push_back(rand()%1000);
@@ -22,6 +22,7 @@ int main(){
     std::cout<<""<<std::endl;
     
     auto start = std::chrono::steady_clock::now();
+    /* it takes 14.34 ms to sort 1000 elements*/
     int j = 0;
     for(int i = 1; i <= size; i++){
         j = i;
@@ -31,6 +32,18 @@ int main(){
         }
     }
 
+/*  it is taking 8 ms to sort 1000 elements  
+    int j = 0;
+    int key;
+    for(int i = 1; i <= size; i++){
+        key = elements[i];
+        j = i;
+        while(key < elements[j-1] && j > 0){
+            elements[j] = elements[j-1];
+            j--;
+        }
+        elements[j] = key;
+    }*/
     auto end = std::chrono::steady_clock::now();
     auto diff = end - start;
     std::cout << std::chrono::duration <double, std::milli> (diff).count() << "ms" << std::endl;
